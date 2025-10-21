b255a1f1b7524e42fc15caafe6c390f9f8fb9305
speedhs <cocattackbased@gmail.com>
2025-10-21 12:45:45 +0530

    working extension

--- include/craphlite.h
+++ include/craphlite.h
@@ -1,8 +1,18 @@
 #ifndef CRAPHLITE_H
 #define CRAPHLITE_H
 
-int craphlite_graph_init(void);
-void craphlite_register_algorithms(sqlite3 *db);
-void craphlite_register_vtable(sqlite3 *db);
+int craphlite_graph_init(void)
+{
+    return 0;
+    // Initializes vertices and edges
+}
+extern void craphlite_register_algorithms(sqlite3 *db){
+    // Placeholder for now
+    // Later this will register graph tables or triggers
+    
+}
+extern void craphlite_register_vtable(sqlite3 *db){
+    // Creates virtual tables
+}
 
 #endif
